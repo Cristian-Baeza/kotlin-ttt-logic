ef94e06bd9d11361db4e7db024270cf9cba4dc4e
Cristian-Baeza <cristianbaeza092@gmail.com>
2021-12-06 15:13:50 -0700

    gitignore file edit/ linter

--- src/main/kotlin/TicTacToeLogic.kt
+++ src/main/kotlin/TicTacToeLogic.kt
@@ -1,11 +1,11 @@
 class TicTacToeLogic {
 
-  var board = arrayOf(
+    var board = arrayOf(
         "available", "available", "available", "available", "available", "available", "available",
         "available", "available"
-  )
+    )
 
-  private val winCombos = arrayOf(
+    private val winCombos = arrayOf(
         arrayOf(0, 1, 2), // top_row
         arrayOf(3, 4, 5), // middle_row
         arrayOf(6, 7, 8), // bottom_row
@@ -14,26 +14,26 @@ class TicTacToeLogic {
         arrayOf(2, 5, 8), // right_column
         arrayOf(0, 4, 8), // left_diagonal
         arrayOf(6, 4, 2) // right_diagonal
-  )
+    )
 
-  private fun turnCount(): Int {
+    private fun turnCount(): Int {
         return board.size - board.count { it == "available" }
-  }
+    }
 
-  fun currentPlayer(): String {
+    fun currentPlayer(): String {
         return if (turnCount() % 2 == 0) "playerOne" else "playerTwo"
-  }
+    }
 
-  fun playMove(spaceToPlay: Int) {
+    fun playMove(spaceToPlay: Int) {
       if (board[spaceToPlay] == "available")
         board[spaceToPlay] = currentPlayer()
-  }
+    }
 
-  fun boardFull(): Boolean {
+    fun boardFull(): Boolean {
       return !board.contains("available")
-  }
+    }
 
-  fun winner(): String? {
+    fun winner(): String? {
       var winner: String? = null
 
       for (win_combo in winCombos) {
@@ -50,9 +50,9 @@ class TicTacToeLogic {
       if (boardFull() && winner == null)
         winner = "tie"
       return winner
-  }
+    }
 
-  fun gameOver(): Boolean {
+    fun gameOver(): Boolean {
       return boardFull() || (winner() != null)
-  }
+    }
 }
